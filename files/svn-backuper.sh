0f0064d9b474d70dd0adedaa4229ccbf1f047a63
Oliver Jehle <ojehle@yahoo.com>
2022-01-30 15:43:49 +0100

    backup only changed repos

--- files/svn-backuper.sh
+++ files/svn-backuper.sh
@@ -9,6 +9,16 @@ BACKUPDIR="/var/svn-backup"
 find $SVNDIR* -maxdepth 0 -type d | while IFS= read -r DIR
 do
 	NAME=`expr match "$DIR" '.*\(/.*\)'`
-	echo "svnadmin dump $DIR > $BACKUPDIR$NAME.dump"
-	svnadmin dump $DIR > $BACKUPDIR$NAME.dump
+	if [ -f $BACKUPDIR$NAME.info ]; then
+      info=$(cat $BACKUPDIR$NAME.info)
+    else
+      info=""
+    fi
+    actual=$(svnlook youngest $DIR)
+    if [ "$actual" != "$info" ] ; then
+	  echo "svnadmin dump $DIR > $BACKUPDIR$NAME.dump"
+      svnadmin dump $DIR > $BACKUPDIR$NAME.dump
+      echo $actual >$BACKUPDIR$NAME.info
+    fi
+
 done
